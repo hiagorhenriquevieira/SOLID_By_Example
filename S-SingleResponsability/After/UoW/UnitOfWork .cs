0f72557960e1a9d7b308004a3a68333fa16740a9
Hiagor Vieira <hiagor.vieira@inovafarma.com.br>
2025-12-08 17:34:00 -0300

    improve: Adds a class for controlling connections and transactions.

---
+++ S-SingleResponsability/After/UoW/UnitOfWork .cs
@@ -0,0 +1,28 @@
+﻿using SOLID_By_Example.S_SingleResponsability.After.Interfaces;
+using System.Data.SqlClient;
+
+namespace SOLID_By_Example.S_SingleResponsability.After.UoW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public UnitOfWork(string connectionString)
+        {
+            Connection = new SqlConnection(connectionString);
+            Connection.Open();
+            Transaction = Connection.BeginTransaction();
+        }
+
+        public SqlConnection Connection { get; }
+        public SqlTransaction Transaction { get; }
+
+
+        public void Commit() => Transaction.Commit();
+
+        public void Rollback() => Transaction.Rollback();
+        public void Dispose()
+        {
+            Transaction.Dispose();
+            Connection.Dispose();
+        }
+    }
+}
